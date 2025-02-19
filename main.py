1f24cb42addd325a73da597f3ff8a7726b396ba4
dawsthehassell <hasselldawson@gmail.com>
2025-02-18 18:40:26 -0600

    check

--- main.py
+++ main.py
@@ -1,2 +1,8 @@
 from tkinter import Tk, BOTH, Canvas
 
+class Window:
+    def __init__(self, width, height):
+        width = self.width
+        height = self.height
+        master = Tk()
+        
