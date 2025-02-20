6756b0603d0c33968f6e0dac56c76e0bda5176fd
dawsthehassell <hasselldawson@gmail.com>
2025-02-20 17:48:34 -0600

    draw move function

--- main.py
+++ main.py
@@ -72,13 +72,36 @@ class Cell:
             line = Line(Point(self._x2, self._y1), Point(self._x2, self._y2))
             self._win.draw_line(line, "black")
 
+    def draw_move(self, to_cell, undo=False):
+        center_x = self._x1 + ((self._x2 - self._x1) / 2)
+        center_y = self._y1 + ((self._y2 - self._y1) / 2)
+        to_center_x = to_cell._x1 + ((to_cell._x2 - to_cell._x1) / 2)
+        to_center_y = to_cell._y1 + ((to_cell._y2 - to_cell._y1) / 2)
+        if undo == False:
+            line = Line(Point(center_x, center_y), Point(to_center_x, to_center_y))
+            self._win.draw_line(line, "red")
+        else:
+            line = Line(Point(center_x, center_y), Point(to_center_x, to_center_y))
+            self._win.draw_line(line, "gray")
+
 def main():
     win = Window(800,600)
-    cell1 = Cell(50, 100, 150, 200, win)
+    # Left cell (as before)
+    cell1 = Cell(50, 50, 100, 100, win)
+    # Right cell (as before)
+    cell2 = Cell(100, 50, 150, 100, win)
+    # Bottom cell (below cell1)
+    cell3 = Cell(50, 100, 100, 150, win)
+
+    # Draw all cells
     cell1.draw()
-    
-    cell2 = Cell(150, 200, 250, 300, win)
     cell2.draw()
+    cell3.draw()
+
+    # Test different moves
+    cell1.draw_move(cell2)  # should draw red line horizontally
+    cell1.draw_move(cell3)  # should draw red line vertically
+    cell2.draw_move(cell3, undo=True)  # should draw gray line diagonally
     win.wait_for_close()
 
 if __name__ == "__main__":
